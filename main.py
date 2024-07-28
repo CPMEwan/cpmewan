583ad3fc9cd453ac56f0cfab3acf85a470f80101
CPMEwan <ewanali74@gmail.com>
2024-07-28 16:56:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ __GROUP_USERNAME__ = "Ewan19_99Ewan"
 
 
 def signal_handler(sig, frame):
-    print("\n Bye Bye...")
+    print("\n Good Bye...")
     sys.exit(0)
 
 def banner(console):
@@ -124,7 +124,7 @@ def rainbow_gradient_string(customer_name):
     for i, char in enumerate(customer_name):
         fraction = i / max(num_chars - 1, 1)
         interpolated_color = interpolate_color(start_color, end_color, fraction)
-        modified_string += f'[{interpolated_color}]{char}'
+        modified_string += '[{interpolated_color}]{char}'
     return modified_string
 
 
