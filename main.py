7e34007b8a052eaf1be0b3542d32622b59e61279
CPMEwan <ewanali74@gmail.com>
2024-07-28 11:51:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -96,8 +96,6 @@ def load_key_data(cpm):
     
     console.print(f"[bold green] Your Telegram ID[/bold green]:[bold cyan] {data.get('telegram_id')}[/bold cyan].")
     
-    console.print(f"[bold green] Your Telegram ID[/bold green]:[bold cyan] {data.get('telegram_username')}[/bold cyan].")
-    
     console.print(f"[bold green] Your Balance $  [/bold green]:[bold cyan] {(data.get('coins') if not data.get('is_unlimited') else 'Unlimited')}[/bold cyan].")
         
     console.print("[bold][red]======================================[/red][/bold]")
