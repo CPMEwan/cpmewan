4c7848805626637bb079ee25a9fe6237d78c792e
CPMEwan <ewanali74@gmail.com>
2024-07-31 23:33:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,9 @@ if __name__ == "__main__":
             console.print("[bold red]{21}[/bold red]: [bold cyan]Change Race Loses[/bold cyan]        [bold green]1.000K[/bold green]")
             console.print("[bold red]{22}[/bold red]: [bold cyan]Clone Account[/bold cyan]            [bold green]5.000K[/bold green]")
             console.print("[bold red]{0} [/bold red]: [bold cyan]Exit[/bold cyan]", end="\n\n")
+            console.print(f"[bold green] Money  [/bold green]:[bold cyan] {data.get('money')}[/bold cyan].")
             service = IntPrompt.ask(f"[bold][?] Select a Service [red][1-{choices[-1]} or 0][/red][/bold]", choices=choices, show_choices=False)
+            console.print(f"[bold green] Money  [/bold green]:[bold cyan] {data.get('money')}[/bold cyan].")
             if service == 0: # Exit
                 console.print(f"[bold yellow][!] Thank You for using our tool, please join our telegram channel[/bold yellow]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue].")
             elif service == 1: # Increase Money
