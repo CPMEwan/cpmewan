527df3ef03a085070ea4221cf0c0b837438819c3
CPMEwan <ewanali74@gmail.com>
2024-08-01 23:54:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ timezonelist = ['UTC','US/Pacific','Europe/Berlin']
 for zone in timezonelist:
 
     now_time = datetime.now(timezone(zone))
-    print now_time.strftime(fmt)
+    
 
 
 def banner(console):
