ade5ab2e90a0fcfe50099b7370b6e29c71dd0844
CPMEwan <ewanali74@gmail.com>
2024-08-01 23:36:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
+import datetime 
+from openpyxl import Workbook 
+import time 
+  
+  
+# Main Function 
+if __name__ == '__main__': 
+    
+    # Create a workbook object 
+    wb = Workbook() 
+  
+    # Select the active sheet 
+    ws = wb.active 
+  
+    # Heading of Cell A1 
+    ws.cell(row=1, column=1).value = "Current Date and Time"
+  
+    # Cell A2 containing the Current Date and Time 
+    ws.cell(row=2, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+  
+    # Sleep of 2 seconds 
+    time.sleep(2) 
+  
+    # Cell A3 containing the Current Date and Time 
+    ws.cell(row=3, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    time.sleep(2)  
+    ws.cell(row=4, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
+    wb.save('gfg.xlsx') 
+    wb.close()
+
 from cpmewan import CPMEwan
 
 __CHANNEL_USERNAME__ = "Ewan1999Ewan"
