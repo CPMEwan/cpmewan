0e4554a3ed41518071010a1dae546a94ffc7df9e
CPMEwan <ewanali74@gmail.com>
2024-08-01 23:38:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,34 +7,6 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
-import datetime 
-import time 
-  
-  
-# Main Function 
-if __name__ == '__main__': 
-    
-    
-  
-    # Select the active sheet 
-    ws = wb.active 
-  
-    # Heading of Cell A1 
-    ws.cell(row=1, column=1).value = "Current Date and Time"
-  
-    # Cell A2 containing the Current Date and Time 
-    ws.cell(row=2, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-  
-    # Sleep of 2 seconds 
-    time.sleep(2) 
-  
-    # Cell A3 containing the Current Date and Time 
-    ws.cell(row=3, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    time.sleep(2)  
-    ws.cell(row=4, column=1).value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
-    wb.save('gfg.xlsx') 
-    wb.close()
-
 from cpmewan import CPMEwan
 
 __CHANNEL_USERNAME__ = "Ewan1999Ewan"
