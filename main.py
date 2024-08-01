24b8e580c9979cd192e092311c74f27c09307288
CPMEwan <ewanali74@gmail.com>
2024-08-01 23:22:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,15 +35,12 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name =  "▓█████  █     █░ ▄▄▄       ███▄    █ \n"
-    brand_name += "▓█   ▀ ▓█░ █ ░█░▒████▄     ██ ▀█   █ \n"
-    brand_name += "▒███   ▒█░ █ ░█ ▒██  ▀█▄  ▓██  ▀█ ██▒\n"
-    brand_name += "▒▓█  ▄ ░█░ █ ░█ ░██▄▄▄▄██ ▓██▒  ▐▌██▒\n"
-    brand_name += "░▒████▒░░██▒██▓  ▓█   ▓██▒▒██░   ▓██░\n"
-    brand_name += "░░ ▒░ ░░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒░   ▒ ▒ \n"
-    brand_name += " ░ ░  ░  ▒ ░ ░    ▒   ▒▒ ░░ ░░   ░ ▒░\n"
-    brand_name += "   ░     ░   ░    ░   ▒      ░   ░ ░ \n"
-    brand_name += "   ░  ░    ░          ░  ░         ░ \n"
+    brand_name =  "▓█████     █     █░    ▄▄▄          ███▄    █ \n"
+    brand_name += "▓█   ▀    ▓█░ █ ░█░   ▒████▄        ██ ▀█   █ \n"
+    brand_name += "▒███      ▒█░ █ ░█    ▒██  ▀█▄     ▓██  ▀█ ██▒\n"
+    brand_name += "▒▓█  ▄    ░█░ █ ░█    ░██▄▄▄▄██    ▓██▒  ▐▌██▒\n"
+    brand_name += "░▒████▒   ░░██▒██▓     ▓█   ▓██▒   ▒██░   ▓██░\n"
+    brand_name += "░░ ▒░ ░   ░ ▓░▒ ▒      ▒▒   ▓▒█░   ░ ▒░   ▒ ▒ "
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
