c9198fdd04d7d3f69ca0c58959b9fc7d4ddb8f28
CPMEwan <ewanali74@gmail.com>
2024-08-03 00:05:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from rich.text import Text
 from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
-from pystyle import Box
+from pystyle import Center
 
 from cpmewan import CPMEwan
 
@@ -57,7 +57,8 @@ def banner(console):
     print(Colorate.Horizontal(Colors.rainbow, '    𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃'))
     print(Colorate.Horizontal(Colors.rainbow, f' ‌           𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}'))
     print(Colorate.Horizontal(Colors.rainbow, '=================================================================='))
-    print(Box.Lines("EWAN KURDISH"))
+
+print(Center.XCenter(Colorate.Horizontal(Colors.rainbow, 'Hello, Welcome to Pystyle')))
 
 def load_player_data(cpm):
     response = cpm.get_player_data()
