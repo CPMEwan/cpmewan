52d073513052b441672850fed49238c3710c8f8c
CPMEwan <ewanali74@gmail.com>
2024-08-02 15:02:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,11 +52,8 @@ def banner(console):
     console.print(colorful_text)
     console.print("[bold][red]==================================================================[/red][/bold]")
     console.print("\t   𝐏𝐋𝐄𝐀𝐒𝐄 𝐋𝐎𝐆𝐎𝐔𝐓 𝐅𝐑𝐎𝐌 𝐂𝐏𝐌 𝐁𝐄𝐅𝐎𝐑𝐄 𝐔𝐒𝐈𝐍𝐆 𝐓𝐇𝐈𝐒 𝐓𝐎𝐎𝐋")
-
 print(Box.Lines(f"Hello, Welcome."))
 print(Box.DoubleCube(f"Hello, Welcome."))
-
-    
     console.print("   [bold][red]  𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃[/bold][red]")
     console.print(f"             [bold green]𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦[/bold green]: [bold cyan]@{__CHANNEL_USERNAME__}[/bold cyan] 𝐎𝐫 [bold cyan]@{__GROUP_USERNAME__}[/bold cyan]") 
     console.print("[bold][red]==================================================================[/red][/bold]")
