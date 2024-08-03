3caae147ff2882b2ab4d22551003419039df899c
CPMEwan <ewanali74@gmail.com>
2024-08-03 13:13:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,124 +1,320 @@
-#!/usr/bin/python
+import os,sys,random
+import platform
+try:
+    import httpx
+    from colr import color
+    from pystyle import Anime as pyAnime
+    from pystyle import Colors as pyColors
+    from pystyle import Colorate as pyColorate
+    from pystyle import Center as pyCenter
+    from pystyle import System as pySystem
+    local_ip = httpx.get('https://api.ipify.org').text
+    response = httpx.get(f"https://ipinfo.io/{local_ip}/json")
+    data_jaringan = response.json()
+except Exception as e:
+    print(f"problem : {e}")
+    input("lets Fix it?... Press Enter")
+    def is_termux():
+        return 'termux' in platform.system().lower()
 
-import random
-from time import sleep
-import os, signal, sys
-from rich.console import Console
-from rich.prompt import Prompt, IntPrompt
-from rich.text import Text
-from rich.style import Style
-import pystyle
-from pystyle import Colors, Colorate
-
-from cpmewan import CPMEwan
-
-__CHANNEL_USERNAME__ = "Ewan1999Ewan"
-__GROUP_USERNAME__   = "Ewan19_99Ewan"
-
-def signal_handler(sig, frame):
-    print("\n Bye Bye...")
-    sys.exit(0)
-
-def gradient_text(text, colors):
-    lines = text.splitlines()
-    height = len(lines)
-    width = max(len(line) for line in lines)
-    colorful_text = Text()
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            if char != ' ':
-                color_index = int(((x / (width - 1 if width > 1 else 1)) + (y / (height - 1 if height > 1 else 1))) * 0.5 * (len(colors) - 1))
-                color_index = min(max(color_index, 0), len(colors) - 1)  # Ensure the index is within bounds
-                style = Style(color=colors[color_index])
-                colorful_text.append(char, style=style)
-            else:
-                colorful_text.append(char)
-        colorful_text.append("\n")
-    return colorful_text
-
-def banner(console):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name =  "▄████▄   ██▓███   ███▄ ▄███▓▓█████  █     █░ ▄▄▄       ███▄    █ \n"
-    brand_name += "▒██▀ ▀█  ▓██░  ██▒▓██▒▀█▀ ██▒▓█   ▀ ▓█░ █ ░█░▒████▄     ██ ▀█   █ \n"
-    brand_name += "▒▓█    ▄ ▓██░ ██▓▒▓██    ▓██░▒███   ▒█░ █ ░█ ▒██  ▀█▄  ▓██  ▀█ ██▒\n"
-    brand_name += "▒▓▓▄ ▄██▒▒██▄█▓▒ ▒▒██    ▒██ ▒▓█  ▄ ░█░ █ ░█ ░██▄▄▄▄██ ▓██▒  ▐▌██▒\n"
-    brand_name += "▒ ▓███▀ ░▒██▒ ░  ░▒██▒   ░██▒░▒████▒░░██▒██▓  ▓█   ▓██▒▒██░   ▓██░\n"
-    brand_name += "░ ░▒ ▒  ░▒▓▒░ ░  ░░ ▒░   ░  ░░░ ▒░ ░░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒░   ▒ ▒ \n"
-    colors = [
-        "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
-        "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
-        "rgb(255,0,255)"
-    ]
-    colorful_text = gradient_text(brand_name, colors)
-    console.print(colorful_text)
-    print(Colorate.Horizontal(Colors.rainbow, '=================================================================='))
-    print(Colorate.Horizontal(Colors.rainbow, '\t         𝐏𝐋𝐄𝐀𝐒𝐄 𝐋𝐎𝐆𝐎𝐔𝐓 𝐅𝐑𝐎𝐌 𝐂𝐏𝐌 𝐁𝐄𝐅𝐎𝐑𝐄 𝐔𝐒𝐈𝐍𝐆 𝐓𝐇𝐈𝐒 𝐓𝐎𝐎𝐋'))
-    print(Colorate.Horizontal(Colors.rainbow, '    𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃'))
-    print(Colorate.Horizontal(Colors.rainbow, f' ‌           𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}'))
-    print(Colorate.Horizontal(Colors.rainbow, '=================================================================='))
-
-def load_player_data(cpm):
-    response = cpm.get_player_data()
-    if response.get('ok'):
-        data = response.get('data')
-        if 'floats' in data and 'localID' in data and 'money' in data and 'coin' in data:
-        
-            print(Colorate.Horizontal(Colors.rainbow, '==========[ PLAYER DETAILS ]=========='))
-            
-            print(Colorate.Horizontal(Colors.rainbow, f' Name   : {(data.get("Name") if "Name" in data else "UNDEFINED")}.'))
-                
-            print(Colorate.Horizontal(Colors.rainbow, f' LocalID: {data.get("localID")}.'))
-            
-            print(Colorate.Horizontal(Colors.rainbow, f' Money  : {data.get("money")}.'))
-            
-            print(Colorate.Horizontal(Colors.rainbow, f' Coins  : {data.get("coin")}.'))
-            
-        else:
-            print(Colorate.Horizontal(Colors.rainbow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
-            exit(1)
+    if is_termux():
+        print("Python is running in Termux.")
+        os.system("pkg update && pkg upgrade -y")
+        os.system("termux-setup-storage -y")
     else:
-        print(Colorate.Horizontal(Colors.rainbow, '! ERROR: seems like your login is not properly set !.'))
-        exit(1)
+        print("Python is running on a Linux PC.")
+    os.system("pip install colr")
+    os.system("pip install httpx")
+    os.system("pip install pystyle")
+    from pystyle import Anime as pyAnime
+    from pystyle import Colors as pyColors
+    from pystyle import Colorate as pyColorate
+    from pystyle import Center as pyCenter
+    from pystyle import System as pySystem
 
+VERSION_CHECK_URL = f"{server_online}/termux-version"
 
-def load_key_data(cpm):
+def get_latest_version_info():
+    try:
+        response = httpx.get(VERSION_CHECK_URL)
+        response.raise_for_status()
+        return response.json()
+    except httpx.RequestError as e:
+        print(f"Error checking for updates: {e}")
+        return None
 
-    data = cpm.get_key_data()
-    
-    print(Colorate.Horizontal(Colors.rainbow, '========[ ACCESS KEY DETAILS ]========'))
-    
-    print(Colorate.Horizontal(Colors.rainbow, f' Access Key : {data.get("access_key")}.'))
-    
-    print(Colorate.Horizontal(Colors.rainbow, f' Telegram ID: {data.get("telegram_id")}.'))
-    
-    print(Colorate.Horizontal(Colors.rainbow, f' Balance $  : {(data.get("coins") if not data.get("is_unlimited") else "Unlimited")}.'))
-        
-    print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐌𝐄𝐍𝐔 ]==============='))
+def download_new_version(download_url, filename):
+    with httpx.Client(follow_redirects=True) as client:
+        response = client.get(download_url)
+        response.raise_for_status()
+        with open(filename, 'wb') as file:
+            file.write(response.content)
 
-def prompt_valid_value(content, tag, password=False):
-    while True:
-        value = Prompt.ask(content, password=password)
-        if not value or value.isspace():
-            print(Colorate.Horizontal(Colors.rainbow, f'{tag} cannot be empty or just spaces. Please try again.'))
+def update_script():
+    version_info = get_latest_version_info()
+    if not version_info:
+        return
+    
+    latest_version = version_info.get("version")
+    download_url = version_info.get("download_url")
+    print(f"CURRENT_VERSION {CURRENT_VERSION}\nlatest_version {latest_version}\ndownload_url {download_url}")
+    if latest_version and download_url:
+        if latest_version != CURRENT_VERSION:
+            print(f"New version available: {latest_version}")
+            print(f"Downloading update... {download_url}")
+            download_new_version(download_url, sys.argv[0])
+            print("Script updated to the latest version. Please restart the script.")
+            exit()
         else:
-            return value
+            print("You already have the latest version.")
+    else:
+        print("Invalid version information received.")
+update_script()
 
-def interpolate_color(start_color, end_color, fraction):
-    start_rgb = tuple(int(start_color[i:i+2], 16) for i in (1, 3, 5))
-    end_rgb = tuple(int(end_color[i:i+2], 16) for i in (1, 3, 5))
-    interpolated_rgb = tuple(int(start + fraction * (end - start)) for start, end in zip(start_rgb, end_rgb))
-    return "{:02x}{:02x}{:02x}".format(*interpolated_rgb)
 
-def rainbow_gradient_string(customer_name):
-    modified_string = ""
-    num_chars = len(customer_name)
-    start_color = "{:06x}".format(random.randint(0, 0xFFFFFF))
-    end_color = "{:06x}".format(random.randint(0, 0xFFFFFF))
-    for i, char in enumerate(customer_name):
-        fraction = i / max(num_chars - 1, 1)
-        interpolated_color = interpolate_color(start_color, end_color, fraction)
-        modified_string += f'[{interpolated_color}]{char}'
-    return modified_string
+def disp(clrnama):
+    clrfirsttime = True
+    clrVnama = clrnama.split("[")
+    clrdisps = clrVnama[0]
+    for clrx in clrVnama:
+        if clrfirsttime == False:
+            clrcode1 = f"{clrx[0:2]}"
+            clrcode2 = f"{clrx[2:4]}"
+            clrcode3 = f"{clrx[4:6]}"
+            clrcode1 = (int(clrcode1, 16))
+            clrcode2 = (int(clrcode2, 16))
+            clrcode3 = (int(clrcode3, 16))
+            clrhuruf = clrx[7:8]
+            clrdisps += color(clrhuruf,
+                              fore=(clrcode1,
+                                    clrcode2,
+                                    clrcode3),
+                              back=(0, 0, 0))
+        if clrfirsttime:
+            clrfirsttime = False
+
+    clrdisps += clrVnama[len(clrVnama)-1][8:len(clrVnama[len(clrVnama)-1])]
+    return(clrdisps)
+
+warnasekarang=""
+def generate(namax):
+    global warnasekarang
+    gabungwarna = ""
+    contohnama = namax
+    # proses memecah huruf di nama
+    data = {
+        "huruf": "",
+        "kodewarna": [255, 0, 0],
+        "mode": 1,
+        "kodewarnaCPM": ""
+    }
+    while True:
+        while True:
+            tanya = random.choice(["merah","kuning","hijau","biru","ungu","pink"])
+            if tanya!=warnasekarang:
+                warnasekarang = tanya
+                break
+        if tanya == "merah":
+            data["kodewarna"] = [255, 0, 0]
+            break
+        elif tanya == "kuning":
+            data["kodewarna"] = [230, 245, 66]
+            break
+        elif tanya == "hijau":
+            data["kodewarna"] = [0, 255, 0]
+            break
+        elif tanya == "biru":
+            data["kodewarna"] = [0, 0, 255]
+            break
+        elif tanya == "ungu":
+            data["kodewarna"] = [150, 66, 245]
+            break
+        elif tanya == "pink":
+            data["kodewarna"] = [245, 66, 215]
+            break
+        else:
+            print("Harus sesuai pilihan warna ..!")
+
+    for huruf in contohnama:
+        while True:
+            # print(f"\nmode sekarang : {data['mode']}")
+            tambah = 45
+            if data["mode"] == 1:
+                if data["kodewarna"][1]+tambah <= 255:
+                    data["kodewarna"][1] += tambah
+                    break
+                else:
+                    data["mode"] += 1
+                    data["kodewarna"] = [255, 255, 0]
+            elif data["mode"] == 2:
+                if data["kodewarna"][0]-tambah >= 0:
+                    data["kodewarna"][0] -= tambah
+                    break
+                else:
+                    data["mode"] += 1
+                    data["kodewarna"] = [0, 255, 0]
+            elif data["mode"] == 3:
+                if data["kodewarna"][2]+tambah >= 255:
+                    data["kodewarna"][2] += tambah
+                    break
+                else:
+                    data["mode"] += 1
+                    data["kodewarna"] = [0, 255, 255]
+            elif data["mode"] == 4:
+                if data["kodewarna"][1]-tambah >= 0:
+                    data["kodewarna"][1] -= tambah
+                    break
+                else:
+                    data["mode"] += 1
+                    data["kodewarna"] = [0, 0, 255]
+            elif data["mode"] == 5:
+                if data["kodewarna"][0]+tambah >= 255:
+                    data["kodewarna"][0] += tambah
+                    break
+                else:
+                    data["mode"] += 1
+                    data["kodewarna"] = [255, 0, 255]
+            elif data["mode"] == 6:
+                if data["kodewarna"][2]-tambah >= 255:
+                    data["kodewarna"][2] -= tambah
+                    break
+                else:
+                    data["mode"] = 1
+                    data["kodewarna"] = [255, 0, 0]
+        # print(f"{huruf} {data['kodewarna']}")
+        gabungwarna += color(huruf,
+                             fore=(data["kodewarna"][0],
+                                   data["kodewarna"][1],
+                                   data["kodewarna"][2]),
+                             back=(0, 0, 0))
+        kodas = []
+        for t in range(3):
+            clrcode = hex(data["kodewarna"][t])[2::]
+            if len(clrcode) == 1:
+                clrcode += "0"
+            kodas.append(clrcode)
+        data["kodewarnaCPM"] += f"[{kodas[0]}{kodas[1]}{kodas[2]}]{huruf}"
+    # print(f"hasil\t:  {disp(data['kodewarnaCPM'])}")
+    # print(f"kode\t:  {data['kodewarnaCPM']}")
+    return data["kodewarnaCPM"]
+def refresh_x():
+    import inspect
+    kucing_garong = inspect.getfile(inspect.currentframe())
+    with open(kucing_garong, 'r') as file:
+        gajah_terbang = file.read()
+        gajah_duduk = len(gajah_terbang)
+    if debug_mode!=1: 
+            pass
+    return gajah_duduk
+pySystem.Clear()
+pySystem.Size(80, 40)
+
+
+text = """
+< [ YouTube TopixSB ] > X < [ ₱ⱤØ₲Ɽ₳₥ ฿Ɇ₮₳ ] >"""[1:]
+
+banner = r"""
+___ç$$$ç________________
+__$$$$$$$_####______####_       YouTube TopixSB
+___*$$$$$$ç####___########        
+_____*$$$$$$$$$$$##########     ▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀█
+_____$$$$$$$$$$$$$##########    ░▒█░░ ░▀▀▀▄▄ ▒█▀▀▄
+______$$$$$$$$$$$$$##########   ░▒█░░ ▒█▄▄▄█ ▒█▄▄█ 
+______$$$$$$$$$$_$$$##########
+______$$$$$$$$$$##$$$##########
+_______$$$$$$$$$_##$$##########
+______$$$$$$$$$$___$$#########
+_____$_$$$$$$$$$$__$$_########
+___$$__$$$$$$$$$$_$$$__######
+______$$$$$$$$$$__$$$___#####
+______$$$$$$$$$___$$____####
+______$$$$$$$$$_________###
+______$$$$$$$$__________##
+_______$$$$$$___________##
+_______$$$$$$______________
+_______$$$$$$$$____________
+_______$$$$$$$$____________
+_______$$$$_$$$$___________
+_______$$$$_$$$$___________
+_______$$$___$$$$__________
+__ççç$$$$$$_çç$$$$__________       
+                          
+           Car Parking Multiplayer Instant Script
+                    LESS THEN 1 MINUTE
+
+                        PRESS ENTER          
+"""[1:]
+
+
+pyAnime.Fade(pyCenter.Center(banner), pyColors.purple_to_red, pyColorate.Vertical, enter=True)
+
+pySystem.Clear()
+
+print("\n"*2    )
+print(pyColorate.Horizontal(pyColors.red_to_yellow, pyCenter.XCenter(text)))
+print("\n"*2)
+
+
+delet=["cpm/pos.py","cpm/__init__.py"]
+for psdd in delet:
+    if os.path.exists(f"{psdd}") == True:
+        os.system(f"rm {psdd}")
+
+
+
+def c(colr, tex):
+    try:
+        w = {
+            "RED": [255, 0, 0],
+            "GREEN": [0, 255, 0],
+            "CYAN": [0, 255, 255],
+            "YELLOW": [255, 255, 0],
+            "GOLD": [255, 223, 0]
+        }
+        return color(tex,
+                     fore=(w[colr.upper()][0],
+                           w[colr.upper()][1],
+                           w[colr.upper()][2]),
+                     back=(0, 0, 0))
+    except:
+        return tex
+    
+def heder():
+        pySystem.Clear()
+        print(f"build : {refresh_x()}")
+        versi_tampil = disp(generate(f"Topix SB CPM TOOLS {CURRENT_VERSION}"))
+        loc_info = f"  Location\t: {data_jaringan.get('city')}, {data_jaringan.get('region')}, {data_jaringan.get('country')}"
+        loc_info = pyColorate.Horizontal(pyColors.green_to_yellow, loc_info)
+        isp_info = f"  ISP     \t: {data_jaringan.get('org')}"
+        isp_info = pyColorate.Horizontal(pyColors.green_to_yellow, isp_info)
+        bannerwz = f"""{c("cyan","=======================================================================")}
+  {versi_tampil} {c("cyan","||")} {c("green","https://carparking.topixsb.dev/")}
+{c("cyan","=======================================================================")}
+{loc_info}
+{isp_info}"""
+        print(bannerwz)
+
+tex="""     IMPORTANT READ
+
+    You must log out of the CPM application first, 
+    unless you only want to use the "Inject Rank" and "Instant Rank" features, 
+    as these two features do not require you to log out.
+
+    Please refill your cash only at https://carparking
+
+"""
+
+print(pyColorate.Horizontal(pyColors.green_to_yellow, pyCenter.XCenter(tex)))
+
+
+
+def warnain(text,inpo=""):
+    tex = f"""{c("cyan","=======================================================================")}"""
+    if inpo:
+        tex+=f"\n\t\t{pyColorate.Horizontal(pyColors.red_to_purple, inpo)}"
+    tex+=f"""
+{pyColorate.Horizontal(pyColors.green_to_yellow, text)}
+{c("cyan","=======================================================================")}"""
 
 if __name__ == "__main__":
     console = Console()
