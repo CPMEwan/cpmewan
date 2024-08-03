f877db94028ac80aca897d579a59f95698f262b7
CPMEwan <ewanali74@gmail.com>
2024-08-03 07:55:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from datetime import datetime
 start_time = datetime.now()
 # do your work here
 end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
+print(f'Duration: {}'.format(end_time - start_time))
 
 from cpmewan import CPMEwan
 
