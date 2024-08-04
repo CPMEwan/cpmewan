0ddcad8b935ea21b04f37e26dff570cc1e85a200
CPMEwan <ewanali74@gmail.com>
2024-08-04 22:09:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def rainbow_gradient_string(customer_name):
     for i, char in enumerate(customer_name):
         fraction = i / max(num_chars - 1, 1)
         interpolated_color = interpolate_color(start_color, end_color, fraction)
-        modified_string += f'[{interpolated_color}]{char}'
+        modified_string += '[{interpolated_color}]{char}'
     return modified_string
 
 if __name__ == "__main__":
