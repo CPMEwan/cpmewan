f6b5a34c137a9fd3ae587e631f696baac9dc7990
CPMEwan <ewanali74@gmail.com>
2024-08-04 22:23:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,9 +108,9 @@ def load_client_details():
     response = requests.get("http://ip-api.com/json")
     data = response.json()
     print(Colorate.Horizontal(Colors.rainbow, '=============[ 𝐋𝐎𝐂𝐀𝐓𝐈𝐎𝐍 ]============='))
-       print(Colorate.Horizontal(Colors.rainbow, f'Ip Address   : {data.get("ip")}.'))
-     print(Colorate.Horizontal(Colors.rainbow, f'Location.       : {data.get("city")} {data.get("regionName")} {data.get("countryCode")}.'))
-    print(Colorate.Horizontal(Colors.rainbow, f'Country         : {data.get("country")} {data.get("zip")}.'))
+    print(Colorate.Horizontal(Colors.rainbow, f'Location.    : {data.get("city")} {data.get("regionName")} {data.get("countryCode")}.'))
+    print(Colorate.Horizontal(Colors.rainbow, f'Country      : {data.get("country")} {data.get("zip")}.'))
+    print(Colorate.Horizontal(Colors.rainbow, f'Ip Address   : {data.get("ip")}.'))
     print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐌𝐄𝐍𝐔 ]==============='))
 
 def interpolate_color(start_color, end_color, fraction):
