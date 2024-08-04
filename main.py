b49146eca86e5273b7621c588f2ebb6b85ca23de
CPMEwan <ewanali74@gmail.com>
2024-08-04 14:04:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def load_player_data(cpm):
 def load_key_data(cpm):
 
     data = cpm.get_key_data()
+        response = requests.get("http://ip-api.com/json")
     data = response.json()
     
     print(Colorate.Horizontal(Colors.rainbow, '========[ ACCESS KEY DETAILS ]========'))
